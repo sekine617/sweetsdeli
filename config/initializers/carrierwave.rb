5a88a3e16d7db12b2dd733dbb65aad80a317dbc9
sekine617 <seweb14wm54@icloud.com>
2021-02-12 14:55:59 +0900

    modify carriewave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,19 @@
-unless Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-      region: 'ap-northeast-1'
-    }
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
-    config.fog_directory  = 'rails-img-2'
-    config.cache_storage = :fog
-  end
+unless Rails.env.development? || Rails.env.test?
+CarrierWave.configure do |config|
+  config.storage :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_directory  = 'rails-img-2'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+    region: 'ap-northeast-1',
+    path_style: true
+  }
 end
+end
+
